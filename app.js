56d473db9f84fc611248b246faadc880f54fc5c2
Dylan Dunn <dhd5076@rit.edu>
2018-09-17 15:26:59 -0400

    Fixed network syncing issues, started input

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 var serv = require('http').Server(app);
-var io = require('socket.io')(serv, {});
-var path = require('path');
+var randomstring = require("randomstring");
+var io = require('socket.io')(serv);
 //Use express to serve the main page
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/client/index.html');
@@ -10,6 +10,10 @@ app.get('/', function(req, res) {
 
 app.get('/play', function(req, res) {
     res.sendFile(__dirname + '/client/play.html');
+});
+
+app.get('/create', function(req, res) {
+    res.redirect('play?gameID=' +  randomstring.generate());
 });
 
 app.get('/editor', function(req, res) {
@@ -22,10 +26,18 @@ app.use('client', express.static(__dirname + '/client'));
 
 //Socket connection handler, print to console on connection
 io.sockets.on('connection', function(socket) {
+    var channel = channel;
     socket.on('joinGame', function(data) {
-        console.log('A User Joined Game:', data);
-        socket.join(data);
-        socket.emit('joinAccepted')
+        channel = data.gameID;
+        socket.join(channel);
+        console.log('A User Joined Game:', channel);
+        socket.emit('joinAccepted', randomstring.generate(4));
+        
+    });
+
+    socket.on('updatePlayers', function(newInfo) {
+        console.log(newInfo);
+        socket.broadcast.to(channel).emit('Update', newInfo);
     });
 });
 
--- public/lib/game/main.js
+++ public/lib/game/main.js
@@ -1,3 +1,8 @@
+var bg;
+var socket;
+var myID;
+var gameInstance;
+
 ig.module( 
 	'game.main' 
 )
@@ -8,15 +13,14 @@ ig.module(
 )
 .defines(function(){
 
-var bg;
-var socket;
-
 MyGame = ig.Game.extend({
 	
 	font: new ig.Font( 'media/04b03.font.png' ),
 	
 	
 	init: function() {
+		ig.input.initMouse();
+		gameInstance = this;
 		var data = [
 			[1,1,1,1,1,1,1,1,1,1],
 			[1,1,1,1,1,1,1,1,1,1],
@@ -28,24 +32,51 @@ MyGame = ig.Game.extend({
 		];
 		bg = new ig.BackgroundMap( 64, data, 'media/grass.png' );
 		bg.zIndex = -1;
-		for(var i = 0; i < 6; i++) {
-			ig.game.spawnEntity(EMan, 10*(10%i), 10*i, { vel: {x: 0, y: 0}});
-		}
 		
 		socket = io();
 		Requests = { QueryString : function(item){
 			var svalue = location.search.match(new RegExp("[\?\&]" + item + "=([^\&]*)(\&?)","i"));
 			return svalue ? svalue[1] : svalue;}}
 
-			var id = Requests.QueryString("gameID");
-			socket.emit('joinGame', id);
-			socket.on('joinAccepted', function(){
+			var gameID = Requests.QueryString("gameID");
+			socket.emit('joinGame', {gameID});
+			socket.on('joinAccepted', function(data){
 				console.log("The server accepted our join");
-				console.log("Switching to game channel " + id + "...");
-				socket.on('channelSwitch', function(data) {
-					console.log("Joined channel:" + data)
-					gatherData();
-				});
+				myID = data;
+				console.log("Switching to game channel " + gameID + " with ID: " + myID);
+				socket.emit('updatePlayers', "SNDINF");
+				for(var i = 0; i < 12; i++) {
+					ig.game.spawnEntity(EMan, Math.random() * 640, Math.random() * 480, {ENTid: i, OWNER: myID});
+					socket.emit('updatePlayers', "ENTUPT:" + gameInstance.entities[i].OWNER + ":" + gameInstance.entities[i].ENTid + ":" + gameInstance.entities[i].pos.x + ":" + gameInstance.entities[i].pos.y);
+				}
+			});
+			socket.on('Update', function(newInfo){
+				console.log("UPDATE" + newInfo);
+				if(newInfo=="SNDINF") {
+					console.log("Someone wants to know who I am.");
+					for(var i = 0; i < gameInstance.entities.length; i++) {
+						//UPDATE ALL ABOUT AN ENTITY, "ENTUPT:USERID:ENTN:XPOS:YPOS:-STATE"
+						if(gameInstance.entities[i].OWNER != myID) {
+							socket.emit('updatePlayers', "ENTUPT:" + gameInstance.entities[i].OWNER + ":" + gameInstance.entities[i].ENTid + ":" + gameInstance.entities[i].pos.x + ":" + gameInstance.entities[i].pos.y);
+						}
+					}
+				}
+				if(newInfo.split(':')[0] == "ENTUPT") {
+					foundMatch = false;
+					for(var i = 0; i < gameInstance.entities.length; i++) {
+						if(gameInstance.entities[i].OWNER == newInfo.split(':')[1] && gameInstance.entities[i].ENTid == newInfo.split(':')[2]) {
+							console.log("MATCH");
+							foundMatch = true;
+							gameInstance.entities[i].pos.x = parseInt(newInfo.split(':')[3]);
+							gameInstance.entities[i].pos.y = parseInt(newInfo.split(':')[4]);
+						}
+					}
+					if(!foundMatch) {
+						console.log("SPAWN");
+						ig.game.spawnEntity(EMan, parseInt(newInfo.split(':')[3]), parseInt(newInfo.split(':')[4]), {ENTid: newInfo.split(':')[2], OWNER: newInfo.split(':')[1]});
+						console.log(gameInstance.entities[i]);
+					}
+				}
 			});
 		},
 	
@@ -56,22 +87,17 @@ MyGame = ig.Game.extend({
 	draw: function() {
 		var x = ig.system.width/2,
 			y = ig.system.height/2;
-			
-		this.font.draw( 'Hello World', x, y, ig.Font.ALIGN.CENTER );
 		bg.draw(0,0);
 		for( var i = 0; i < this.entities.length; i++ ) {
 			this.entities[i].draw();
+			this.font.draw( 'OWNER:' + this.entities[i].OWNER, this.entities[i].pos.x, this.entities[i].pos.y, ig.Font.ALIGN.CENTER );
 		}
+		this.font.draw("OUR ID: " + myID,100, 10, ig.Font.ALIGN.CENTER);
+		this.font.draw("MOUSE X: " + ig.input.mouse.x,100, 20, ig.Font.ALIGN.CENTER);
+		this.font.draw("MOUSE Y: " + ig.input.mouse.y,100, 20, ig.Font.ALIGN.CENTER);
 	}
 });
 
 ig.main( '#canvas', MyGame, 144, 640, 448, 2);
 
 });
-
-function gatherData(){
-	socket.emit('sendDataPlz', {color: 'RED'})
-	socket.on('hereData', function(data){
-		console.log('Someone sent us : ' + data);
-	});
-}
